baca375913e56f103a4351dcd7626711b9ba4e84
Tomer Abramovich <untradeta@gmail.com>
2020-05-23 18:38:38 +0300

    added gitlab supports

--- src/main.rs
+++ src/main.rs
@@ -128,15 +128,123 @@ fn download_and_save_from_github(url: &str, output_filename: &str, verbose: bool
 	write_urls(&urls, output_filename, compare_list, prepand_command);
 }
 
+fn gitlab_get_subgroups(group_url: &str) -> Vec<u64> {
+	let client = reqwest::blocking::Client::new();
+
+	let resp = client.get(group_url)
+		.header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0")
+		.header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+		.header("Accept-Language", "en-US,en;q=0.5")
+		//.header("Accept-Encoding", "gzip, deflate, br")
+		.send()
+		.unwrap();
+	
+
+	if resp.status() != reqwest::StatusCode::OK {
+		panic!("failed to get response code 200 from: {}, instead got: {:?}", group_url, resp.status());
+	}
+
+	let repos: serde_json::Value = serde_json::from_str(&resp.text().unwrap()).unwrap();
+	let repos_array = repos.as_array().expect("result is not array");
+	let mut groups = Vec::new();
+
+	for repo in repos_array {
+		if let Some(repo) = repo.as_object() {
+			let id = repo.get("id")
+						.expect("should have 'id' attribute")
+						.as_u64()
+						.expect("id should be an integer");
+
+			groups.push(id);
+		} else {
+			panic!("repo should be an object");
+		}
+	}
+
+	groups
+}
+
+fn gitlab_get_repositories(group_url: &str) -> Vec<String> {
+	let client = reqwest::blocking::Client::new();
+
+	let resp = client.get(group_url)
+		.header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0")
+		.header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+		.header("Accept-Language", "en-US,en;q=0.5")
+		//.header("Accept-Encoding", "gzip, deflate, br")
+		.send()
+		.unwrap();
+	
+
+	if resp.status() != reqwest::StatusCode::OK {
+		panic!("failed to get response code 200 from: {}, instead got: {:?}", group_url, resp.status());
+	}
+
+	let repos: serde_json::Value = serde_json::from_str(&resp.text().unwrap()).unwrap();
+	let repos_array = repos.as_array().expect("result is not array");
+	let mut urls = Vec::new();
+
+	for repo in repos_array {
+		if let Some(repo) = repo.as_object() {
+			urls.push(String::from(repo.get("http_url_to_repo").expect("should have 'http_url_to_repo' attribute")
+				.as_str().expect("http_url_to_repo should be a string")));
+		} else {
+			panic!("repo should be an object");
+		}
+	}
+
+	urls
+}
+
+fn gitlab_to_list(group_name: &str) -> Vec<String> {
+	let mut url_list = Vec::new();
+
+	let group_api_url = format!("https://gitlab.com/api/v4/groups/{}/subgroups", group_name);
+	let subgroups = gitlab_get_subgroups(&group_api_url);
+	//println!("{:?}", subgroups);
+
+	for group_id in subgroups {
+		let mut urls = gitlab_to_list(&group_id.to_string());
+		url_list.append(&mut urls);
+	}
+
+	let repositories_api_url = format!("https://gitlab.com/api/v4/groups/{}/projects", group_name);
+	let mut repositories = gitlab_get_repositories(&repositories_api_url);
+	//println!("{:?}", repositories);
+	url_list.append(&mut repositories);
+
+	url_list
+}
+
+fn download_and_save_from_gitlab(group_name: &str, output_filename: &str, compare_file: &str, prepand_command: &str) {
+	let urls = gitlab_to_list(group_name);
+	let mut compare_list = Vec::new();
+
+	if !compare_file.is_empty() {
+		let mut file = File::open(compare_file).unwrap();
+		let mut contents = String::new();
+		file.read_to_string(&mut contents).unwrap();
+
+		contents = contents.replace("\r", "");
+		
+		for line in contents.split("\n") {
+			compare_list.push(line.trim().to_string());
+		}
+	}
+
+	write_urls(&urls, output_filename, compare_list, prepand_command);
+}
+
 enum Action {
 	None,
 	DownloadGithub,
+	DownloadGitlab,
 	ScanReposDir
 }
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	println!("{:?}", args);
+	//println!("{:?}", args);
 
 	let mut prepand_command = String::new();
 	let mut output_filename = String::new();
@@ -147,7 +255,8 @@ fn main() {
 	let mut filter_forks = false;
 	let mut only_forks = false;
 	let mut cmd = Action::None;
-
+	let mut gitlab_group = String::new();
+	
 	for (i, arg) in args.iter().enumerate() {
 		match arg.as_str() {
 			"-d" | "--default" => {
@@ -178,6 +287,10 @@ fn main() {
 				url = get_github_url("users", args.get(i + 1).unwrap());
 				cmd = Action::DownloadGithub;
 			}
+			"--gitlab-group" => {
+				gitlab_group = args.get(i + 1).unwrap().clone();
+				cmd = Action::DownloadGitlab;
+			}
 			"--scan-repos" => {
 				reposdir = args.get(i + 1).unwrap().clone();
 				cmd = Action::ScanReposDir;
@@ -201,6 +314,9 @@ fn main() {
 		}
 		Action::DownloadGithub => {
 			download_and_save_from_github(url.as_str(), output_filename.as_str(), false, filter_forks, only_forks, &compare_file, prepand_command.as_str());
+		}
+		Action::DownloadGitlab => {
+			download_and_save_from_gitlab(&gitlab_group, output_filename.as_str(), &compare_file, prepand_command.as_str());
 		}
 		Action::ScanReposDir => {
 			unimplemented!();
